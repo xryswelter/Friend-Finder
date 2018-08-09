c518073b8c51b22835b95cef916653728ad1b65f
Xrys <xryswelter@gmail.com>
2018-08-08 21:09:56 -0700

    initial commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.export = (app)=>{
+
+    app.get('/', (req,res)=>{
+        res.sendFile(path.join(__dirname,'../public/home.html'));
+    });
+    app.get('/survey', (req,res)=>{
+        res.sendFile(path.join(__dirname,'../public/survey.html'));
+    });
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+//Required Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });  
