c22b02a3473f5c4520e83bf405e78edc920fe418
Xrys <xryswelter@gmail.com>
2018-08-11 22:58:20 -0700

    submission prototype

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Friend-Finder
+
+## What the project does
+This project is used to demonstrate my ability to use Express, Node, deployment to Heroku.
+## Why the project is useful
+This can be helpful since new dating apps are being made all the time and figuring out how to opimize searches as well as managing databases that best fit the client's survey results.
+## How users can get started with the project
+You will need to launch the application through node to see if find it in the local host or through Heroku.  Upon completing the survey, your results will indicate who is your best fit for a friend.
+##Who maintains and contributes to the project
+Maintainer: Chris Welter
+Contributors: Assistance by Vivek Reddy, Chris Berber, Leeland Clenista
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 let friends = require("../data/friends");
-const path = require('path');
 
 module.exports = (app) => {
     app.get('/api/friends', function (req, res) {
@@ -8,9 +7,9 @@ module.exports = (app) => {
     app.post('/api/friends', (req, res) => {
         let body = req.body;
         //   body = JSON.stringify(body);
-        console.log("body = " + body.newProfile);
+        // console.log("body = " + body.newProfile);
         let result = friend(body)
-        console.log(result);
+        // console.log(result);
         res.json(result);
     });
 
@@ -28,17 +27,17 @@ module.exports = (app) => {
             let fScore = 0;
             for (let j = 0; j < friendz.length; j++) {
                 let echo = Math.abs(friendz[j] - parseInt(var1[j]));
-                console.log(friendz[j], var1[j]);
+                // console.log(friendz[j], var1[j]);
                 //reduce the values (took from internet)
                 fScore += echo;
             }
-            console.log('fs = '+fScore);
+            // console.log('fs = '+fScore);
             fResults.push(fScore);
         }
         // Indexing using the Math.min function to find the lowest difference b/n user and friends
-        console.log('fResults = ' + fResults)
+        // console.log('fResults = ' + fResults)
         let index = fResults.indexOf(Math.min(...fResults));
-        console.log('index'+index);
+        // console.log('index'+index);
         //Return the location of the friends to be the best match 
         return friends[index];
     };
